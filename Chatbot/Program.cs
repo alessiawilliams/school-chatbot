5edd559df7dc31e993b366b8b2b1170181384dfc
Alessia Williams <contact@alessia.tech>
2020-01-07 12:04:00 +0000

    Call menu

--- Chatbot/Program.cs
+++ Chatbot/Program.cs
@@ -4,9 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Chatbot bot = new Chatbot();
-            
-            // TODO: Call Menu()
+            Menu();
+            ChatEnvironment();
         }
     }
 }
